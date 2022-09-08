677d2c4ee027158ce432bd6f42adf400dd112140
CrazyCollin <lazycollinl@gmail.com>
2022-09-08 21:32:38 +0800

    Update something

--- algorithm/sort/main.go
+++ algorithm/sort/main.go
@@ -4,7 +4,9 @@ import "fmt"
 
 func main() {
 	arr := []int{5, 2, 3, 1}
-	quickSort(arr, 0, len(arr)-1)
+	//quickSort(arr, 0, len(arr)-1)
+	//mergeSort(arr, 0, len(arr)-1)
+	heapSort(arr)
 	for _, num := range arr {
 		fmt.Printf("%d ", num)
 	}
